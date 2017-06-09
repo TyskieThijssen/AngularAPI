a654c190896d788b600d70892d8f0c874bda8528
tyskie <thijs.thijssen1@hotmail.com>
2017-06-09 20:28:12 +0200

    Hello World application

---
+++ src/main/java/com/tyskie/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.tyskie;
+
+/**
+ * Created by Thijs on 9-6-2017.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
